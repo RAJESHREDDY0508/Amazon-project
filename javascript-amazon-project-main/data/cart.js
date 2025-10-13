980577f0df5370c300b3daf878fa1eaed5a7e849
rajeshreddy689-jpg <rajeshreddy689@gmail.com>
2025-10-13 11:57:45 -0500

    Save Data and Generate HTML for Delivery Options

--- javascript-amazon-project-main/data/cart.js
+++ javascript-amazon-project-main/data/cart.js
@@ -1,11 +1,13 @@
 export let cart = JSON.parse(localStorage.getItem('cart'));
 if(!cart){
-  [{
+  cart = [{
     productId: 'e43638ce-6aa0-4b85-b27f-e1d07eb678c6',
-    qunatity: 2,
+    quantity: 2,
+    deliveryOptionId: '1'
   },{
     productId: '15b6fc6f-327a-4ec4-896f-486349e85a3d',
-    qunatity: 1
+    quantity: 1,
+    deliveryOptionId: '2'
   }];
 }
 
@@ -22,12 +24,14 @@ export function addToCart(productId){
       matchingItem = cartItem;
     }
   });
+
   if(matchingItem){
-    matchingItem.qunatity += 1;
+    matchingItem.quantity += 1;
   }else{
     cart.push({
       productId : productId,
-      qunatity: 1
+      quantity: 1,
+      deliveryOptionId: '1'
     });
   }
 
