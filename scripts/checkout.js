206b08f205e296a8b2005ecd61e7af3098e5f085
rajeshreddy689-jpg <rajeshreddy689@gmail.com>
2025-10-14 15:40:00 -0500

    Learn promises and convert code to Promises

--- scripts/checkout.js
+++ scripts/checkout.js
@@ -1,10 +1,49 @@
 import { renderOrderSummary } from "./checkout/orderSummary.js";
 import { renderPaymentSummary } from "./checkout/paymentSummary.js";
 import { loadProducts } from "../javascript-amazon-project-main/data/products.js";
+import { loadCart } from "../javascript-amazon-project-main/data/cart.js";
 // import '../javascript-amazon-project-main/data/cart-class.js'
 //import '../javascript-amazon-project-main/data/backend-practice.js';
 
-loadProducts(() => {
+Promise.all([
+  new Promise((resolve) => {
+    loadProducts(() => {
+      resolve('value1');
+    });
+  }),
+  new Promise((resolve) => {
+    loadCart(() => {
+      resolve();
+    });
+  })
+
+]).then((values) => {
+  console.log(values);
   renderOrderSummary();
   renderPaymentSummary();
 });
+
+// new Promise((resolve) => {
+//   loadProducts(() => {
+//     resolve('value1');
+//   });
+
+// }).then((value) => {
+//   console.log(value);
+//   return new Promise((resolve) => {
+//     loadCart(() => {
+//       resolve();
+//     });
+//   });
+
+// }).then(() => {
+//   renderOrderSummary();
+//   renderPaymentSummary();
+// });
+
+// loadProducts(() => {
+//   loadCart(() => {
+//   renderOrderSummary();
+//   renderPaymentSummary();
+//   });
+// });
