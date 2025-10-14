220e9eeb8c0abb8bec464550c3d7df11ba037d75
rajeshreddy689-jpg <rajeshreddy689@gmail.com>
2025-10-14 14:38:46 -0500

    Add backedn practice file

--- scripts/checkout.js
+++ scripts/checkout.js
@@ -1,6 +1,7 @@
 import { renderOrderSummary } from "./checkout/orderSummary.js";
 import { renderPaymentSummary } from "./checkout/paymentSummary.js";
 // import '../javascript-amazon-project-main/data/cart-class.js'
+import '../javascript-amazon-project-main/data/backend-practice.js';
 
 renderOrderSummary();
 renderPaymentSummary();
