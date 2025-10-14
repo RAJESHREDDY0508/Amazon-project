5270d54daf425ecbcdb57715c91e18bbfcb51d81
rajeshreddy689-jpg <rajeshreddy689@gmail.com>
2025-10-14 11:43:17 -0500

    Convert products into classes

--- scripts/checkout.js
+++ scripts/checkout.js
@@ -1,6 +1,6 @@
 import { renderOrderSummary } from "./checkout/orderSummary.js";
 import { renderPaymentSummary } from "./checkout/paymentSummary.js";
-import '../javascript-amazon-project-main/data/cart-class.js'
+// import '../javascript-amazon-project-main/data/cart-class.js'
 
 renderOrderSummary();
 renderPaymentSummary();
